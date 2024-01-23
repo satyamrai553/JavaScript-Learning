058815771e050258e976ea10a064dae0c82d8d0f
Satyam Rai <71927837+satyamrai553@users.noreply.github.com>
2024-01-23 21:04:00 +0000

    Date in javascript

--- 01_Basic/07_Date.js
+++ 01_Basic/07_Date.js
@@ -14,7 +14,7 @@ let myCreatedDate = new Date("01-14-2023")
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
+console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
