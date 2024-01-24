b0e7e9267ab572644cf606df793743dde1ae0361
Satyam Rai <71927837+satyamrai553@users.noreply.github.com>
2024-01-24 10:57:04 +0000

    array 2 revised

--- 02_Basic/02_Arrays.js
+++ 02_Basic/02_Arrays.js
@@ -4,19 +4,19 @@ const dc_heros = ["superman", "flash", "batman"]
 // marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]); //throw an error because array is not present
 
 // const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+// console.log(allHeros); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array); //[ 1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
 
 
 
