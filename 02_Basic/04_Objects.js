25cd8d5d8ddad64cf01d2cf7306ae19abb300b03
Satyam Rai <71927837+satyamrai553@users.noreply.github.com>
2024-01-24 11:15:58 +0000

    revised object

--- 02_Basic/04_Objects.js
+++ 02_Basic/04_Objects.js
@@ -5,7 +5,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+console.log(tinderUser);
 
 const regularUser = {
     email: "some@gmail.com",
@@ -17,7 +17,7 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
@@ -27,7 +27,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+console.log(obj3);
 
 
 const users = [
